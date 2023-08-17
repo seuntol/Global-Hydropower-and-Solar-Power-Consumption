f700f50e4141bf43d87fee9103674ebcaae32129
seuntol <142135004+seuntol@users.noreply.github.com>
2023-08-17 15:06:47 -0400

    Create ETL_code_Alter_table.sql

---
+++ SQL_Code/ETL_code_Alter_table.sql
@@ -0,0 +1,6 @@
+SQL Alter table to change entity column name from ï»¿Entity  to the desired apperance: Entity
+
+```sql
+ALTER TABLE hydropower_consumption_dim
+    CHANGE ï»¿Entity Entity VARCHAR(50);
+```
